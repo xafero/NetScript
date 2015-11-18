e0aea3e4e3e457642908ba6e24596503c7c1ba39
xafero <georg.berendt@uni-oldenburg.de>
2015-11-18 02:31:42 +0100

    Added a stub for Roslyn stuff

---
+++ NetScript.Impl.Mca/McaHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.CodeAnalysis.Scripting;
+using Microsoft.CodeAnalysis.Scripting.CSharp;
+using Microsoft.CodeAnalysis.Scripting.VisualBasic;
+using NetScript.API;
+
+namespace NetScript.Impl.Mca
+{
+	public static class McaHelper
+	{
+		public static Func<string,Script> CreateScript(string lang)
+		{
+			switch (lang) {
+				case "VB":
+				case "Visual Basic":
+					return c => VisualBasicScript.Create(c);
+				case "C#":
+				case "CSharp":
+					return c => CSharpScript.Create(c);
+				default:
+					throw new Exception(string.Format("Unknown compiler '{0}'!", lang));
+			}
+		}
+	}
+}
---
+++ NetScript.Impl.Mca/McaScriptHost.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.CodeAnalysis.Scripting;
+using Microsoft.CodeAnalysis.Scripting.CSharp;
+using Microsoft.CodeAnalysis.Scripting.VisualBasic;
+using NetScript.API;
+
+namespace NetScript.Impl.Mca
+{
+	public class McaScriptHost : IScriptHost
+	{
+		public McaScriptHost(string lang)
+		{
+			
+		}
+		
+		public void Dispose()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
